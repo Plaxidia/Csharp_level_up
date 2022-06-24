6f18d8650459b4117d0b222d54a6bf0c9422e183
plaxidiahove@gmail.com <plaxidiahove@gmail.com>
2022-06-24 17:23:17 +0300

    added if else statement

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Csharp
+{
+    class program
+    {
+        static void Main(string [] args)
+        {
+            var input = Console.ReadLine();
+
+            if (input ==" Hello")
+            {
+                Console.WriteLine(" Hello, How are you");
+            }
+            else if(input == "Hey,quick question")
+                {
+                Console.WriteLine("What's your name");
+                }
+            else
+            {
+                Console.WriteLine(" all i do is to greet ");
+            }
+        }
+    }
+}
+
