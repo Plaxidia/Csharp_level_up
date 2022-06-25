eb954b1da54cc70c56d290653c49fe50ab641d77
plaxidiahove@gmail.com <plaxidiahove@gmail.com>
2022-06-25 22:10:31 +0300

    added a pratise task(game)

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -60,7 +60,7 @@ namespace Csharp
             {
                 Console.WriteLine(index);
             }
-              */
+             
             //strings 
             var index = 0;
             var Name = " Hove";
@@ -69,8 +69,65 @@ namespace Csharp
                 Console.WriteLine(" they call me " + Name + "number "+ index);
                 index++;
             }
+            */
+
+            //lets practice
+            //personal Greeter
+            //1.must allow the user to play as much as they want
+            //2. must greet  differently depending on the age
+            //3.must tell the user how many years untill the reach 100 years old
 
 
+            //var input = Console.ReadLine();
+            bool isRunning = true;
+            while (isRunning)
+            {
+                string name;
+                int age;
+
+                Console.WriteLine("welcome! whats your name");
+                name = Console.ReadLine();
+
+                Console.WriteLine($"Hello { name}, What is your age?");
+                var myage = Console.ReadLine();
+                age = Convert.ToInt32(myage);
+
+                switch(age)
+                {
+                    case var theAge when theAge < 10:
+                        {
+                                Console.WriteLine("Hello Baby,you are too young to play this game.");
+                                 break;
+                        }
+                    case var theAge when theAge < 18 && theAge >= 10:
+                        {
+                            Console.WriteLine(" You are too young to play this game ,go home.");
+                            break;
+                        }
+                    case var theAge when theAge < 70 && theAge >= 18:
+                        {
+                            Console.WriteLine(" Thanks for entering your information ,enjoy the game.");
+                            break;
+                        }
+                    case var theAge when theAge < 100 && theAge >= 70:
+                        {
+                            Console.WriteLine(" Thanks for entering your information ,but you are too old for the game game.");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine(" Thanks ,Not an usual age.");
+                            break;
+                        }
+                }
+                Console.WriteLine($"You are {100 -age} years until you are 100.");
+                Console.WriteLine(" Do you want to run again");
+                string isRunningResponse = Console.ReadLine();
+                if (isRunningResponse == "no")
+                {
+                    isRunning = false;
+                }
+            }
         }
     }
 }
