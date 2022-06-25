39105d8cb25a1dee976df5302209ace682393567
plaxidiahove@gmail.com <plaxidiahove@gmail.com>
2022-06-25 22:44:56 +0300

    Added a new file

---
+++ Csharp/function.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+           // funtion is args block of code that execute a statement
+        }
+     } 
+}
