06d1639f7926f3d09133062dcd39ffb48420c954
plaxidiahove@gmail.com <plaxidiahove@gmail.com>
2022-06-26 04:13:48 +0300

    Inheritance

--- Csharp/Stat.cs
+++ Csharp/Stat.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Csharp
-{
+{/*
       class Program
         {
             static void Main(string[] args)
@@ -30,5 +30,5 @@ namespace Csharp
             }
 
         }
-    
+    */
 }
---
+++ Csharp/inherit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle =new Vehicle();
+            Van van = new Van();
+            Car car = new Car();
+
+            car.model = " BMW";
+            //Console.WriteLine(Car);
+
+
+        }
+
+    }
+   
+   
+    class Vehicle
+    {
+        public bool haswheels = true;
+        public string Make;
+        public string model;
+    }
+    class Van:Vehicle
+    {
+        public int seatRaws;
+        public bool adjustableseats;
+           
+    }
+    public class Car:Vehicle
+    {
+        public bool Stearing;
+    }
+}
